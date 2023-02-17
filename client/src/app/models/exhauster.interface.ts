6b4ba84bd04e2b724db0c4fb859b116b73585773
Danil Makushev <reurairin@gmail.com>
2023-02-17 22:50:40 +0200

    change interfaces

--- client/src/app/models/exhauster.interface.ts
+++ client/src/app/models/exhauster.interface.ts
@@ -2,9 +2,11 @@ import { Sensor } from './sensor.interface';
 
 export interface Exhauster {
   name: string;
-  rotor: string;
-  rotorLastChanged: number;
-  rotorChangeExpected: number;
-  hasCritical: boolean;
-  sensors: Sensor[];
+  //   rotor: string;
+  //   rotorLastChanged: number;
+  //   rotorChangeExpected: number;
+  hasAlarm: boolean;
+  hasWarning: boolean;
+  bearings: Sensor[];
+  aux_items: Sensor[];
 }
